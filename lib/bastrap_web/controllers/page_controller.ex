3e734a34da745b88deb591e342f7c03cde3e6ee8
Berkan Unal <Berkanunal@gmail.com>
2024-09-17 15:25:37 +0300

    Delete unused page modules comes builtin with phx.new

--- lib/bastrap_web/controllers/page_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule BastrapWeb.PageController do
-  use BastrapWeb, :controller
-
-  def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
-  end
-end
