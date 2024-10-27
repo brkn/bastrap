c814eeddf637fff1dbb4316fff45d9400053b088
Berkan Unal <Berkanunal@gmail.com>
2024-10-27 23:49:59 +0300

    Styling - Fix center pile not wrapping list items woth <ol>

--- lib/bastrap_web/live/game/center_pile_component.ex
+++ lib/bastrap_web/live/game/center_pile_component.ex
@@ -6,11 +6,11 @@ defmodule BastrapWeb.Game.CenterPileComponent do
   def render(assigns) do
     ~H"""
     <div id="center-pile" class="bg-green-600 rounded-lg p-4 flex flex-col items-center space-y-4">
-      <div class="flex justify-center items-center space-x-2">
+      <ol class="flex justify-center items-center space-x-2">
         <%= for {card, index} <- Enum.with_index(@center_pile.cards) do %>
           <CenterPileCardComponent.render id={"center-pile-card-#{index}"} card={card} index={index} />
         <% end %>
-      </div>
+      </ol>
     </div>
     """
   end
